fbb5b1e78cb0e6f8642b7e5f5ab5894795a350f0
jacyu99 <itsjacquelineyu@gmail.com>
2024-06-05 20:06:57 -0700

    replaced last_year_count_commits with NULLIF()

--- transform/models/marts/fact_commits_monthly.sql
+++ transform/models/marts/fact_commits_monthly.sql
@@ -3,7 +3,7 @@ SELECT
   c.repo_id,
   COUNT(*) AS count_commits,
   LAG(count_commits, 12) OVER (PARTITION BY c.repo_id ORDER BY d.date_month) AS last_year_count_commits,
-  (count_commits/last_year_count_commits) - 1 AS yoy_growth,  
+  (count_commits / NULLIF(last_year_count_commits, 0)) - 1 AS yoy_growth,  
 FROM {{ ref('dim_date') }} AS d 
   LEFT JOIN {{ ref('fact_commits') }} AS c
   ON d.date_month = DATE_TRUNC('month', c.date)
--- transform/models/marts/fact_stars_monthly.sql
+++ transform/models/marts/fact_stars_monthly.sql
@@ -3,7 +3,7 @@ SELECT
   s.repo_id, 
   COUNT(*) AS count_stars,
   LAG(count_stars, 12) OVER (PARTITION BY s.repo_id ORDER BY d.date_month) AS last_year_count_stars,
-  (count_stars/last_year_count_stars) - 1 AS yoy_growth,  
+  (count_stars / NULLIF(last_year_count_stars, 0)) - 1 AS yoy_growth,  
 FROM {{ ref('dim_date') }} AS d 
   LEFT JOIN {{ ref('fact_stars') }} AS s 
   ON d.date_month = DATE_TRUNC('month', s.date)
